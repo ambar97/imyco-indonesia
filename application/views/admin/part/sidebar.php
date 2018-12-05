b5f81bde0d7fc4a5c1ea7e0220fc78ad55d49eee
DESKTOP-FAMSFKF\bahrularif <bahrularif007@gmail.com>
2018-12-05 13:43:37 +0700

    new action just view

---
+++ application/views/admin/part/sidebar.php
@@ -0,0 +1,92 @@
+<div id="sidebar" class="sidebar">
+			<!-- begin sidebar scrollbar -->
+			<div data-scrollbar="true" data-height="100%">
+				<!-- begin sidebar user -->
+				<ul class="nav">
+					<li class="nav-profile">
+						<a href="javascript:;" data-toggle="nav-profile">
+							<div class="cover with-shadow"></div>
+							<div class="image">
+								<img src="<?php echo base_url()?>/master/admin/assets/img/user/user-13.jpg" alt="" />
+							</div>
+							<div class="info">
+								<b class="caret pull-right"></b>
+								Sean Ngu
+								<small>Front end developer</small>
+							</div>
+						</a>
+					</li>
+					<li>
+						<ul class="nav nav-profile">
+							<li><a href="javascript:;"><i class="fa fa-cog"></i> Settings</a></li>
+							<li><a href="javascript:;"><i class="fa fa-pencil-alt"></i> Send Feedback</a></li>
+							<li><a href="javascript:;"><i class="fa fa-question-circle"></i> Helps</a></li>
+						</ul>
+					</li>
+				</ul>
+				<!-- end sidebar user -->
+				<!-- begin sidebar nav -->
+				<ul class="nav">
+					<li class="nav-header">Navigation</li>
+					<li class="has-sub active">
+						<a href="javascript:;">
+							<b class="caret"></b>
+							<i class="fa fa-th-large"></i>
+							<span>Dashboard</span>
+						</a>
+					</li>
+					</li>
+					<li class="has-sub">
+	
+						<ul class="sub-menu">
+							<li><a href="email_inbox.html">Inbox</a></li>
+							<li><a href="email_compose.html">Compose</a></li>
+							<li><a href="email_detail.html">Detail</a></li>
+						</ul>
+					</li>
+					<li class="has-sub">
+						<a href="javascript:;">
+							<b class="caret"></b>
+							<i class="fa fa-table"></i>
+							<span>Tables</span>
+						</a>
+						<ul class="sub-menu">
+							<li><a href="table_basic.html">Basic Tables</a></li>
+							<li class="has-sub">
+							  <a href="javascript:;"><b class="caret pull-right"></b> Managed Tables</a>
+								<ul class="sub-menu">
+									<li><a href="table_manage.html">Default</a></li>
+									<li><a href="table_manage_autofill.html">Autofill</a></li>
+									<li><a href="table_manage_buttons.html">Buttons</a></li>
+									<li><a href="table_manage_colreorder.html">ColReorder</a></li>
+									<li><a href="table_manage_fixed_columns.html">Fixed Column</a></li>
+									<li><a href="table_manage_fixed_header.html">Fixed Header</a></li>
+									<li><a href="table_manage_keytable.html">KeyTable</a></li>
+									<li><a href="table_manage_responsive.html">Responsive</a></li>
+									<li><a href="table_manage_rowreorder.html">RowReorder</a></li>
+									<li><a href="table_manage_scroller.html">Scroller</a></li>
+									<li><a href="table_manage_select.html">Select</a></li>
+									<li><a href="table_manage_combine.html">Extension Combination</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li class="has-sub">
+						<a href="javascript:;">
+							<b class="caret"></b>
+							<i class="fa fa-image"></i>
+							<span>Gallery</span>
+						</a>
+						<ul class="sub-menu">
+							<li><a href="gallery.html">Gallery v1</a></li>
+							<li><a href="gallery_v2.html">Gallery v2</a></li>
+						</ul>
+					</li>
+					<!-- begin sidebar minify button -->
+					<li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="fa fa-angle-double-left"></i></a></li>
+					<!-- end sidebar minify button -->
+				</ul>
+				<!-- end sidebar nav -->
+			</div>
+			<!-- end sidebar scrollbar -->
+		</div>
